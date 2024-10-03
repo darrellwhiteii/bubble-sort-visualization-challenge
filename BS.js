29b7f9f8d91e08d327061eb08ec390f000ad5249
Darrell <whiteiidarrell@gmail.com>
2024-10-03 11:02:21 -0500

    First Comit: Let's Go!

---
+++ BS.js
@@ -0,0 +1,97 @@
+// // CONST | No Solution
+
+// const
+// const
+// const
+
+// let nosolution = false; // Initialize the variable
+
+// // FUNCTIONS
+
+
+// function name(params) {
+    
+// }
+// function name(params) {
+    
+// }
+// function name(params) {
+    
+// }
+
+// // FUNCTION WITHIN FUNCTION WITH IF STATEMENTS
+
+// function name(params) {
+//     function name(params) {
+        
+//     }
+// function name(params) {
+//     function name(params) {
+//         if (condition) {
+            
+//         }
+//         if (condition) {
+            
+//         }
+//     }
+// }
+
+// // FOR CONDITIONALS
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// // WHILE CONDITIONALS
+
+// while (condition) {
+    
+// }
+
+// while (condition) {
+    
+// }
+
+// // DO WHILE CONDITIONALS
+
+// do {
+    
+// } while (condition);
+
+// do {
+    
+// } while (condition);
+
+// // BASIC IF STATEMENTS
+
+// if (condition) {
+    
+// }
+
+// if (condition) {
+    
+// }
+
+// // NO SOLUTION: CALL FUNCTION
+
+// function setup() {
+//     createCanvas(400, 400);
+//   }
+  
+//   function draw() {
+//     background(220);
+    
+//     if (nosolution) {
+//       console.log("No solution found.");
+//     } else {
+//       console.log("Solution exists.");
+//     }
+//   }
+  
